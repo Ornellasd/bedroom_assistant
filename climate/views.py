0551281d44d0abd1121dee0e429b904a31329f72
David Ornellas <ornellasd@gmail.com>
2018-04-24 11:24:10 -0700

    implements outside weather forecast

--- climate/views.py
+++ climate/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from . import weather_forecast
+from .weather_forecast import *
+
 
 def index(request):
     """The home page for bedroom-assistant"""
-    return render(request, 'climate/index.html')
+    
+    return render(request, 'climate/index.html', {'outside_temp': outside_temp, 'status': status, 'icon_url': icon_url})
 
---
+++ climate/weather_forecast.py
@@ -0,0 +1,13 @@
+import pyowm
+
+
+owm = pyowm.OWM('96bae3ab3550d3c6c1bde626c2a458e4')
+city_id = 5746545
+
+obs = owm.weather_at_id(city_id)
+w = obs.get_weather()
+get_temp = w.get_temperature('fahrenheit')['temp']
+outside_temp = str(get_temp)[:2] + "°F"
+status = w.get_detailed_status().title()
+icon = w.get_weather_icon_name()
+icon_url = 'http://openweathermap.org/img/w/' + icon + '.png'
