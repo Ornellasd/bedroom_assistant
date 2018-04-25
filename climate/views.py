0e31244419387feda24ce15041c7f128d09017bc
David Ornellas <ornellasd@gmail.com>
2018-04-25 10:32:06 -0700

    refactors view using HttpResponseRedirect

--- climate/views.py
+++ climate/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from . import weather_forecast, wemo
 from .weather_forecast import *
@@ -12,22 +13,20 @@ def index(request):
     icon_url = get_forecast()[2] 
     wemo_state = get_status()
 
-    return render(request, 'climate/index.html', {'outside_temp': outside_temp, 'status': status, 'icon_url': icon_url, 'wemo_state':wemo_state})
+    context = {
+        'outside_temp': outside_temp, 
+        'status': status,
+        'icon_url': icon_url,
+        'wemo_state':wemo_state    
+    }
+    
+    return render(request, 'climate/index.html', context)
 
 def desk_on(request):
     on = wemo_on()
-    wemo_state = get_status()
-    outside_temp = get_forecast()[0]
-    status = get_forecast()[1]    
-    icon_url = get_forecast()[2] 
-       
-    return render(request, 'climate/index.html', {'on': on, 'wemo_state': wemo_state, 'outside_temp': outside_temp, 'status': status, 'icon_url': icon_url})
-
+    return HttpResponseRedirect('/')
+    
 def desk_off(request):
     off = wemo_off()
-    wemo_state = get_status()
-    outside_temp = get_forecast()[0]
-    status = get_forecast()[1]    
-    icon_url = get_forecast()[2] 
-       
-    return render(request, 'climate/index.html', {'off': off, 'wemo_state': wemo_state, 'outside_temp': outside_temp, 'status': status, 'icon_url': icon_url})
+    return HttpResponseRedirect('/')
+    
